d4fdd0581c5c4bc9c68ebb10892b9520227ff324
pthaunoo <95956442+pthaunoo@users.noreply.github.com>
2021-12-20 10:14:10 +0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,16 @@
+<?php
+
+$Welcome = "Mo Expense Tracker";
+
+?>
+
+<html>
+    <head>
+        <title>
+            php
+        </title>
+</head>
+<body>
+    <h1><?php echo $welcome; ?></h1>
+</body>
+</html>
